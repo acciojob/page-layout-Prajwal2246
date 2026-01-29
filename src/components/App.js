60d9972e94909be3927080fe0ba6674594c4b024
Prajwal2246 <janbandhuprajwal28@gmail.com>
2026-01-29 11:22:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import PageLayout from "./PageLayout";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <PageLayout
+        header={<h1>Welcome to my website</h1>}
+        footer={<h2>©2023 My Website.All rights reserved.</h2>}
+      >
+        <p>This is the content of my website.</p>
+      </PageLayout>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
